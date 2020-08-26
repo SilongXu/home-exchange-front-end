57406cdcf204bc033db5ac4f38c9262912caa6e6
yanxi.wang <yanxi.wang@transwarp.io>
2020-08-26 10:28:42 +0800

    refactor: WARP-49616 menu pages

---
+++ src/menu/menu-abstract.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="menu-abstract">
+    Abstract
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuAbstract',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
